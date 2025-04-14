c6d61500f6ef23c4ad8b20af3be9f7c0adfa7eaf
Pavel <saintcavvy@gmail.com>
2025-04-14 20:16:13 +0300

    player movement added
    simple movement script to add WASD movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float ControlSpeed;
     Vector2 movement;
 
-    void Start()
-    {
-        
-    }
-
     void Update()
-    {   
-        float xOffset = movement.x * ControlSpeed * Time.deltaTime;
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, 0f, 0f);
+    {
+        ProcessTranslation();
     }
 
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * ControlSpeed * Time.deltaTime;
+        float yOffset = movement.y * ControlSpeed * Time.deltaTime;
+        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+    }
 
     public void OnMove(InputValue value)
     {
