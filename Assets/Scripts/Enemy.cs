a06e24b314ead32f321746b77aa4376f5c4abc01
Pavel <saintcavvy@gmail.com>
2025-04-16 19:40:39 +0300

    tuning and refactoring a bit
    comments added to scripts;
    clamping offset on y axis added;
    player ship size changed;

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject destroyVFX;
+
+
+
+    //this method provides enemy death mechanic via collision with player weapon particles
     private void OnParticleCollision(GameObject other)
     {
         Instantiate(destroyVFX, transform.position, Quaternion.identity);
