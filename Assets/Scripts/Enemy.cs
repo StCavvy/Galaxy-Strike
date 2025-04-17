14b0c30fc191a9d222bb0dc985e40d5de4297fc8
Pavel <saintcavvy@gmail.com>
2025-04-17 18:48:40 +0300

    ScoreBoard mechanic added
    - added scoreboard script;
    -added scoreboard UI element;

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,14 +2,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private byte hitPoints = 6;
     [SerializeField] private GameObject destroyVFX;
+    [SerializeField] private byte scoreValue = 10;
+
+    ScoreBoard scoreboard;
 
 
+    private void Start()
+    {
+        scoreboard = FindFirstObjectByType<ScoreBoard>();
+    }
 
-    //this method provides enemy death mechanic via collision with player weapon particles
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyVFX, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    //this method provides enemy health and death mechanic via collision with player weapon particles
+    private void ProcessHit()
+    {
+        hitPoints--;
+        if (hitPoints <= 0)
+        {
+            scoreboard.IncreaseScore(scoreValue);
+            Instantiate(destroyVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
